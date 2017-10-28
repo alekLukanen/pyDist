c88c2c8f53d688b95cb73f6ee7648fe32c69963e
alekLukanen <alukanen@icloud.com>
2017-10-28 14:35:19 -0500

    update ip for client

--- testing/NodeStarter.py
+++ testing/NodeStarter.py
@@ -30,4 +30,6 @@ def start_two_nodes(server_ip='0.0.0.0'
 
 if __name__ == '__main__':
     #one node
-    node = start_single_node()
+    node = start_single_node(server_ip='192.168.0.15'
+                             , server_port=9000, client_ip='192.168.0.21'
+                             , client_port=9001)
