ad7af66b319dc1bf58364ad14b32f8e72e60064b
alekLukanen <alukanen@icloud.com>
2017-11-01 21:48:51 -0500

    create tasks from dictionary

--- Tasks.py
+++ Tasks.py
@@ -5,6 +5,8 @@ Created on Wed Nov  1 21:16:01 2017
 
 @author: lukanen
 """
+import NodeInterface
+import pickleFunctions
 
 class BaseTask(object):
     
@@ -37,3 +39,29 @@ class BaseTask(object):
                 }
         return dictionary
     
+    def create_from_dictionary(self, dictionary):
+        if ('node_interface' in dictionary):
+            self.node_interface = NodeInterface.NodeInterface()
+            self.node_interface.create_from_dictionary(dictionary['node_interface'])
+        else:
+            self.node_interface = None
+            
+        self.file_name = dictionary['file_name'] if 'file_name' in dictionary else ''
+        self.function_name = dictionary['function_name'] if 'function_name' in dictionary else ''
+        
+        self.arguements = pickleFunctions.unPickle(dictionary['arguements'].encode('latin1')) if 'arguements' in dictionary else ()
+        self.return_value = pickleFunctions.unPickle(dictionary['return_value'].encode('latin1')) if 'return_value' in dictionary else ''
+        
+        if ('cluster_trace' in dictionary):
+            for node_dict in dictionary['cluster_trace']:
+                node = NodeInterface.NodeInterface()
+                node.create_from_dictionary(node_dict)
+                self.cluster_trace.append(node)
+        else:
+            self.cluster_trace = []
+            
+        self.finished = dictionary['finished'] if 'finished' in dictionary else False
+        
+        
+        
+        
