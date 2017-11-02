8459132174b33b550b3845f8a56d660889b79d6d
Alek Lukanen <alukanen@icloud.com>
2017-11-02 13:02:39 -0500

    updated nodes and tasks

---
+++ Nodes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 12:27:31 2017
+
+@author: alek
+"""
+
+import asyncio
+import NodeInterface
+
+class ClusterExecutorNode(object):
+    
+    def __init__(self):
+        self.interface = NodeInterface.NodeInterface()
+        self.server_interfaces = []
+        self.client_interfaces = []
+        
+        self.node_id_tick = 0
+        self.job_id_tick = 0
+        
+    def increment_id_tick(self):
+        self.node_id_tick+=1
+        
+    def increment_job_id_tick(self):
+        self.job_id_tick+=1
+        
+    def get_address(self):
+        return "http://%s:%d" % (self.ip, self.port)
+    
+    def convert_to_dictionary(self):
+        dictionary = {
+                'interface': self.interface.convert_to_dictionary(),
+                'server_interfaces': [sInt.convert_to_dictionary() for sInt in self.server_interfaces],
+                'client_interfaces': [cInt.convert_to_dictionary() for cInt in self.client_interfaces],
+                'node_id_tick': self.node_id_tick,
+                'job_id_tick': self.job_id_tick
+                }
+        return dictionary
+        
+    def create_from_dictionary(self, dictionary):
+        if ('interface' in dictionary):
+            self.interface = NodeInterface.NodeInterface()
+            self.interface.create_from_dictionary(dictionary['interface'])
+        else:
+            self.interface = None
+            
+        
+        
+        
+        
+    
+    
+    
+    
--- Tasks.py
+++ Tasks.py
@@ -8,6 +8,7 @@ Created on Wed Nov  1 21:16:01 2017
 import NodeInterface
 import pickleFunctions
 
+#the base task will be used as a simple version of the full task.
 class BaseTask(object):
     
     def __init__(self):
@@ -18,6 +19,7 @@ class BaseTask(object):
         
         self.arguements = ()
         self.return_value = None
+        self.exception = None
         
         self.cluster_trace = []
         self.finished = False
@@ -36,11 +38,13 @@ class BaseTask(object):
                 'function_name': self.function_name,
                 'arguements': pickleFunctions.createPickle(self.arguements).decode('latin1'),
                 'return_value': pickleFunctions.createPickle(self.return_value).decode('latin1'),
+                'exception': pickleFunctions.createPickle(self.exception.decode('latin1')),
                 'cluster_trace': [self.convert_obj_to_dictionary(node) for node in self.cluster_trace],
                 'finished': self.finished,
                 'job_id': pickleFunctions.createPickle(self.job_id).decode('latin1')
                 }
         return dictionary
+    
     
     def create_from_dictionary(self, dictionary):
         if ('node_interface' in dictionary):
@@ -51,6 +55,7 @@ class BaseTask(object):
             
         self.file_name = dictionary['file_name'] if 'file_name' in dictionary else None
         self.function_name = dictionary['function_name'] if 'function_name' in dictionary else None
+        self.exception = pickleFunctions.unPickle(dictionary['exception'].encode('latin1')) if 'exception' in dictionary else None
         
         self.arguements = pickleFunctions.unPickle(dictionary['arguements'].encode('latin1')) if 'arguements' in dictionary else ()
         self.return_value = pickleFunctions.unPickle(dictionary['return_value'].encode('latin1')) if 'return_value' in dictionary else None
@@ -64,9 +69,47 @@ class BaseTask(object):
             self.cluster_trace = []
         
         self.finished = dictionary['finished'] if 'finished' in dictionary else False
-                                  
+        
         self.job_id = pickleFunctions.unPickle(dictionary['job_id'].encode('latin1')) if 'job_id' in dictionary else ''
         
         
+class Task(BaseTask):
+    
+    def __init__(self):
+        BaseTask.__init__(self)
+        self.cancel = False
+        self.cancelled = False
+        self.callbacks = []
         
+    def cancel(self):
+        print ('cancel()')
+        self.cancel = True
+        #try and cancle the process here
+        return self.cancelled
+        
+    def cancelled(self):
+        print ('cancelled()')
+        return self.cancelled
+    
+    def done(self):
+        print ('done()')
+        return self.finished
+    
+    def result(self):
+        print ('result()')
+        return self.return_value
+    
+    def exception(self):
+        print ('exception()')
+        return self.exception
+    
+    def add_done_callback(self, fn):
+        print ('add_done_callback()')
+        self.callbacks.append(fn)
+        
+    #other functions here
+        
+    
+    
+    
         
