571e91cca60085911926b82209f0d2035ae0d7e8
alekLukanen <alukanen@icloud.com>
2017-11-03 16:15:14 -0500

    aiohttp added

--- Nodes.py
+++ Nodes.py
@@ -8,7 +8,13 @@ Created on Thu Nov  2 12:27:31 2017
 
 import asyncio
 import NodeInterface
+from aiohttp import web
 
+async def index(request):
+    print ('here')
+    return web.Response(text='Hello Aiohttp!')
+
+    
 class ClusterExecutorNode(object):
     
     def __init__(self):
@@ -16,8 +22,17 @@ class ClusterExecutorNode(object):
         self.server_interfaces = []
         self.client_interfaces = []
         
+        self.ip = '0.0.0.0'
+        self.port = 9000
+        
         self.node_id_tick = 0
         self.job_id_tick = 0
+        
+        self.loop = asyncio.get_event_loop()
+
+        self.app = web.Application(loop=self.loop)
+        self.app.router.add_route('GET', '/', index)
+        web.run_app(self.app, host=self.ip, port=self.port)
         
     def increment_id_tick(self):
         self.node_id_tick+=1
@@ -49,7 +64,9 @@ class ClusterExecutorNode(object):
         
         
         
-    
-    
+if __name__=='__main__':
+    print ('starting a ClusterExecutorNode...')
+    node = ClusterExecutorNode()
+    node.boot('0.0.0.0', 9000)
     
     
