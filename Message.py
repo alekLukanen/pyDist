a92a1e6e93e00449513a61748a9730d68a8397fa
alekLukanen <alukanen@icloud.com>
2017-12-19 09:59:07 -0600

    updated message objects

--- Message.py
+++ Message.py
@@ -6,10 +6,10 @@ Created on Fri Dec 15 22:20:15 2017
 @author: lukanen
 """
 
-class MessageBase(object):
+class StackMessage(object):
     
-    def __init__(self):
-        self.data = []
+    def __init__(self, data=[]):
+        self.data = data
         self.cluster_trace = []
         self.name = 'MessageBase'
         
@@ -18,6 +18,12 @@ class MessageBase(object):
         
     def getItem(self):
         return self.data.pop()
+    
+    def getLength(self):
+        return len(self.data)
+    
+    def __str__(self):
+        return ("len(data): %d, len(cluster_trace)" % (len(self.data),len(self.cluster_trace)))
 
 class StringMessage(object):
     
@@ -36,3 +42,19 @@ class StringMessage(object):
     def __str__(self):
         return 'message: %s' % self.message
             
+    
+if __name__ == '__main__':
+    print ('Message testing...')
+    
+    print ('stack message')
+    s_msg = StackMessage(data=[1,2,3])
+    s_msg.addItem(4)
+    
+    for _ in range(0, s_msg.getLength()):
+        print ('s_msg.pop(): ', s_msg.getItem())
+        
+    print ('string message')
+    str_msg = StringMessage(message='this is the message')
+    print ('str_msg: ', str_msg)
+    
+    
