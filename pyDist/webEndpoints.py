0f66bbe60e42bd8c9fc0813cb58e707d19345973
alekLukanen <alukanen@icloud.com>
2018-11-06 18:18:41 -0600

    made simple changes

--- pyDist/webEndpoints.py
+++ pyDist/webEndpoints.py
@@ -6,4 +6,9 @@ node = None
 
 @aiohttp_jinja2.template('index.jinja2')
 async def splash_page(request):
-    return {'hello': f'world: {node.interface.num_queued}'}
+    return {'interface_num_cores': node.interface.num_cores,
+            'interfaces_num_users': len(node.interfaces.user_interfaces),
+            'interfaces_server_interfaces': len(node.interfaces.server_interfaces),
+            'interfaces_client_interfaces': len(node.interfaces.client_interfaces)}
+#.server_interfaces = []
+#        self.client_interfaces = []
