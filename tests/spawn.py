3f9422e6812123e11a2fa5f2919ff607e633bc49
alekLukanen <alukanen@icloud.com>
2018-12-26 11:43:55 -0600

    added core count to spawn file

--- tests/spawn.py
+++ tests/spawn.py
@@ -7,20 +7,21 @@ import pyDist.endpointSetup
 
 
 print('--- spawning a Node ---')
-print(f'|- node ip : {sys.argv[1]} ')
-print(f'|- node port : {sys.argv[2]}')
+print(f'|- num cores : {int(sys.argv[1])}')
+print(f'|- node ip : {sys.argv[2]} ')
+print(f'|- node port : {sys.argv[3]}')
 
-node = pyDist.endpointSetup.setup_cluster_node(num_cores=4)
-node.set_ip_and_port(sys.argv[1], int(sys.argv[2])) # need to set for node connections
+node = pyDist.endpointSetup.setup_cluster_node(num_cores=int(sys.argv[1]))
+node.set_ip_and_port(sys.argv[2], int(sys.argv[3]))  # need to set for node connections
 
-num_connections = int((len(sys.argv)-3)/2)
+num_connections = int((len(sys.argv)-4)/2)
 
 for i in range(0, num_connections):
-    ip_index = 3+2*i
-    port_index = 4+2*i
+    ip_index = 4+2*i
+    port_index = 5+2*i
     print(f'|- connect to ip : {sys.argv[ip_index]}')
-    print(f'|- connect to port : {sys.argv[port_index]}')
+    print(f'|- connect to port : {int(sys.argv[port_index])}')
     node.connect_to_node(sys.argv[ip_index], int(sys.argv[port_index]))
 
 # boot the node with the given connections
-node.boot(sys.argv[1], int(sys.argv[2]))
+node.boot(sys.argv[2], int(sys.argv[3]))
